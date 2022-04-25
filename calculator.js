4ff4518cc95443352799a6c41b6462a94226a180
Raul Jimenez <rjbarco@gmail.com>
2022-04-25 09:34:36 +0200

    Functions for basic math operators are added

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// Simple operation functions
+// Functions receive strings but need to operate with numbers (conversion needed)
+function sum(a,b) {
+    return Number(a) + Number(b);
+}
+
+function sub(a,b) {
+    return Number(a) - Number(b);
+}
+
+function mult(a,b) {
+    return Number(a) * Number(b);
+}
+
+function div(a,b) {
+    return Number(a) / Number(b);
+}
